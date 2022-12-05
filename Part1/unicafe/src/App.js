9adb2728ef61d5b50d7440bd7c7340dc545c6e35
francasabianca <103165239+francasabianca@users.noreply.github.com>
2022-12-05 15:27:45 -0300

    Add files via upload

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,58 +1,88 @@
 import {useState} from 'react'
 
 const Title = props => <h1>{props.text}</h1>
-const Display = props => <p>{props.text} {props.counter}</p>
-const Button = (props) => {
+const Subtitle = props => <h3>{props.text}</h3>
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button> 
+const StatisticLine = (props) => <tr><td>{props.text}</td><td>{props.counter}</td></tr>
+
+const Statistics = (props) => {
+
+  if(props.counterTotal == 0) {
+    return (
+      <> 
+        <Subtitle text = 'No feedback given' />
+      </>
+    )
+  }
+
   return (
-    <button onClick={props.onClick}>{props.text}</button>
+    <table>
+      <tbody>
+        <StatisticLine text='good' counter = { props.counterGood } />
+        <StatisticLine text='neutral' counter = { props.counterNeutral } />
+        <StatisticLine text='bad' counter = { props.counterBad } />
+        <StatisticLine text='Total' counter = { props.counterTotal } />
+        <StatisticLine text='average' counter = { props.averageNumber } />
+        <StatisticLine text='positive' counter = { props.feedbackPercentage + '%' } />
+      </tbody>      
+    </table>
   )
-} 
+}
+
+const Buttons = (props) => {
+
+  return (
+    <>
+      <Button text='good' onClick = {props.sumGood} />
+      <Button text='bad' onClick = {props.sumBad} />
+      <Button text='neutral' onClick = {props.sumNeutral} />
+    </>
+  )
+}
 
 const App = () => {
-  
   const [counterGood, setCounterGood] = useState(0)
   const [counterNeutral, setCounterNeutral] = useState(0)
   const [counterBad, setCounterBad] = useState(0)
   const [counterTotal, setCounterTotal] = useState(0)
-  const [counterAverage, setCounterAverage] = useState(0)
+  const [averageNumber, setAverageNumber] = useState(0)
   const [feedbackPercentage, setFeedbackPercentage] = useState(0)
 
   const sumGood = () => {
-    setCounterGood(counterGood + 1)
-    setCounterTotal(counterTotal + 1)
-    setCounterAverage((counterTotal + 1) / 3)
-    setFeedbackPercentage(((counterTotal + 1) / 100) * counterGood + 1)
-  }
+    let newCounterGood = counterGood + 1
+    let newCounterTotal = counterTotal + 1
+    setCounterGood(newCounterGood)
+    setCounterTotal(newCounterTotal)
+    setAverageNumber((newCounterTotal) / 3)
+    setFeedbackPercentage(((newCounterGood) / newCounterTotal) * 100) 
+    }
+  
   const sumNeutral = () => {
-    setCounterNeutral(counterNeutral + 1)
-    setCounterTotal(counterTotal + 1)
-    setCounterAverage(counterTotal / 3)
-    setFeedbackPercentage(((counterTotal + 1) / 100) * counterGood)
-  }
-
-  const sumBad = () => {
-    setCounterBad(counterBad + 1)
-    setCounterTotal(counterTotal + 1)
-    setCounterAverage(counterTotal / 3)
-    setFeedbackPercentage(((counterTotal + 1) / 100) * counterGood)
+    let newCounterNeutral = counterNeutral + 1
+    let newCounterTotal = counterTotal + 1
+    setCounterNeutral(newCounterNeutral)
+    setCounterTotal(newCounterTotal)
+    setAverageNumber(newCounterTotal / 3)
+    setFeedbackPercentage(((counterGood) / newCounterTotal) * 100)
   }
   
+  const sumBad = () => {
+    let newCounterBad = counterBad + 1
+    let newCounterTotal = counterTotal + 1
+    setCounterBad(newCounterBad)
+    setCounterTotal(newCounterTotal)
+    setAverageNumber(newCounterTotal / 3)
+    setFeedbackPercentage(((counterGood) / newCounterTotal) * 100)
+  }
+
   return (
     <div>
-      <Title text={'give feedback'} />
-      <Button text='good' onClick={sumGood} />
-      <Button text='neutral' onClick={sumNeutral} />
-      <Button text='bad' onClick={sumBad} />
-      <Title text={'statistics'} />
-      <Display text='good' counter={counterGood} />
-      <Display text='neutral' counter={counterNeutral} />
-      <Display text='bad' counter={counterBad} />
-      <Display text='Total' counter={counterTotal} />
-      <Display text='average' counter={counterAverage}/>
-      <Display text='positive' counter={feedbackPercentage}/>
+      <Title text = 'give feedback' />
+      <Buttons sumGood = {sumGood} sumBad = {sumBad} sumNeutral = {sumNeutral}/>
+      <Title text = 'Statistics' />
+      <Statistics counterBad = {counterBad} counterGood = {counterGood} counterNeutral = {counterNeutral} counterTotal = {counterTotal} averageNumber = {averageNumber} feedbackPercentage = {feedbackPercentage} />
     </div>
   )
-
 }
 
 export default App;
