98cc2d129741438fcfd2fc003a6d7085f9614d81
francasabianca <103165239+francasabianca@users.noreply.github.com>
2022-12-09 23:04:10 -0300

    Add files via upload

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -3,11 +3,11 @@ const Person = ({ person }) => {
 }
 
 const Persons = ({ persons }) => {
-    
+
     return (
         <div>
             <ul>
-                    {persons.map(person => <Person key={person.id} person={person} />)} :
+                {persons.map(person => <Person key={person.id} person={person} />)}
             </ul>
         </div>
     )
