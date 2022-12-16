549819c4de5af531bdf484403e73f400624f14da
francasabianca <103165239+francasabianca@users.noreply.github.com>
2022-12-16 14:35:22 -0300

    Add files via upload

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+app.use(express.json())
+app.use(morgan(':method :url :status - :response-time ms :body'))
 //data returned in get method
 
 let persons =
@@ -27,32 +30,69 @@ let persons =
     }
 ]
 
-app.use(express.json())
+const personNameAlreadyExists = (name) => {
+  const personExists = persons.find(p => p.name === name)
+  return personExists
+}
 
-//get method that returns a json
-
-app.get('/api/persons', (request, response) => {
-    console.log(persons)
-    response.json(persons)
-})
-
+//GET method that returns number of persons in phonebook
 app.get('/api/info', (request, response) => {
-    const personsNumber = persons.length
-    const date = new Date()
-    response.send(
-        `<h1>Phonebook has info for ${personsNumber} persons</h1>` +
-        `<h1>${date}</h1>`
-    )
+  const personsNumber = persons.length
+  const date = new Date()
+  response.send(
+    `<h1>Phonebook has info for ${personsNumber} persons</h1>` +
+    `<h1>${date}</h1>`
+  )
 })
 
+//GET method that return all resources
+app.get('/api/persons', (request, response) => {
+  console.log(persons)
+  response.json(persons)
+})
+
+//GET method that return a resource by id
 app.get('/api/persons/:id', (request, response) => {
-    //return the corresponding resource or a 404 status (not found)
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-    console.log(id, typeof id)
-    console.log(person)
-    
-    person ? response.json(person) : response.status(404).end()
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)  
+  person ? response.json(person) : response.status(404).end()
+})
+
+//DELETE method that removes a resource by id
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const id =  Math.floor(Math.random() * 1000)
+  const body = request.body
+
+  if(!body || !body.number || !body.name) {
+    return response.status(400).json(
+      {
+        error: 'content misssing'
+      }
+    )
+  } else if (personNameAlreadyExists(body.name)) {
+    return response.status(400).json(
+      {
+        error: 'name must be unique'
+      }
+    )
+  }
+
+  const person = {
+    personId : id,
+    name: body.name,
+    number: body.number
+  }
+  
+  persons = persons.concat(person)
+  response.json(person)
+  morgan.token('body', request => JSON.stringify(request.body))
 })
 
 const PORT = 3001
