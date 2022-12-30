6c70f2afcfc04d52b89eb85eb8be76454c9c5878
francasabianca <103165239+francasabianca@users.noreply.github.com>
2022-12-30 13:42:12 -0300

    Add files via upload

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -49,12 +49,15 @@ app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
   const person = {
-    content: body.content,
-    important: body.important,
+    name: body.name,
+    number: body.number,
   }
+
+  console.log('person', person, 'id', request.params.id)
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
+      console.log('updated person', updatedPerson)
       response.json(updatedPerson)
     })
     .catch(error => next(error))
